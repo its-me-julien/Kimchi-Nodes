162651cc83ae117561d979326d537decf1d97932
Jay N T <jp.tollie@gmail.com>
2025-02-06 16:37:08 +0800

    kimchi

--- index.js
+++ index.js
@@ -60,9 +60,5 @@ setTimeout(function(){
       $("#loading").removeClass("animated fadeOut");
       $("#loading").css("display","none");
       $("#box").css("display","none");
-      $("#projects").removeClass("animated fadeIn");
-      $("#about").removeClass("animated fadeIn");
-      $("#contact").removeClass("animated fadeIn");
-      $("#work").removeClass("animated fadeIn");
     },1000);
 },1500);
